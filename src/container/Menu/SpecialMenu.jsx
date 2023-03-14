c5895c7d1a98ba8898590c8656905c099b6b8f83
Felix <felixtwoli21@gmail.com>
2023-03-14 20:35:11 +0300

    Add SpecialMenu component and related CSS styles

--- src/container/Menu/SpecialMenu.jsx
+++ src/container/Menu/SpecialMenu.jsx
@@ -1,30 +1,44 @@
-import React from 'react';
+import React from "react";
 
-import { SubHeading, MenuItem } from '../../components';
-import { images, data } from '../../constants';
+import { SubHeading, MenuItem } from "../../components";
+import { images, data } from "../../constants";
 
-import './SpecialMenu.css';
+import "./SpecialMenu.css";
 
 const SpecialMenu = () => (
-  <div className='app__specialMenu flex__center setion__padding' id='menu'>
-    <div className='app__specialMenu-title'>
-      <SubHeading title='Menu that fits your plate' />
-      <h1 className='headtext__cormorant'>Today's Special</h1>
+  <div className="app__specialMenu flex__center setion__padding" id="menu">
+    <div className="app__specialMenu-title">
+      <SubHeading title="Menu that fits your plate" />
+      <h1 className="headtext__cormorant">Today's Special</h1>
     </div>
 
-    <div className='app__specialMenu-menu'>
-      <div className='app__specialMenu-menu__wine flex__center'>
-        <p className='app__specialMenu_menu_heading'>Wine and Beer</p>
-        <div className='app__specialMenu_menu_items'>
+    <div className="app__specialMenu-menu">
+      <div className="app__specialMenu-menu__wine flex__center">
+        <p className="app__specialMenu_menu_heading">Wine and Beer</p>
+        <div className="app__specialMenu_menu_items">
           {data.wines.map((wine, index) => (
             <p>{wine.title}</p>
-            
           ))}
-
-
         </div>
       </div>
+
+      <div className="app__specialMenu-menu_img">
+        <img src={images.menu} alt="menu img" />
+      </div>
+
+      <div className="app__specialMenu-menu__cocktails flex__center">
+        <p className="app__specialMenu_menu_heading">Wine and Beer</p>
+        <div className="app__specialMenu_menu_items">
+          {data.cocktails.map((cocktail, index) => (
+            <p>{cocktail.title}</p>
+          ))}
+        </div>
+      </div>
+
     </div>
+        <div style={{marginTop: '15px'}}>
+          <button type="button" className="custom__button">View More</button>
+        </div>
   </div>
 );
 
